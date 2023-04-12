d5aa52d5b44b9c80028761a11b6b044b0be0ad8e
Manan Sharma <manansharma177@gmail.com>
2023-04-12 12:13:12 +0530

    new commit

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode("Jaipur", (error, data) => {
-    console.log("Error: ", error)
-    console.log("Data: ", data)
-})
+const address = process.argv[2]
 
-forecast(26.915458, 75.818982, (error, data) => {
-    console.log("Error: ", error)
-    console.log("Data: ", data)
-})
+if (address) {
+    geocode(address, (error, { longitude, latitude, location } = {}) => {
+        if (error) {
+            return console.log("Error: ", error)
+        }
+        forecast(longitude, latitude, (error, forecastData) => {
+            if (error) {
+                return console.log("Error: ", error)
+            }
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+} else {
+    console.log("Please provide an address")
+}
+
+
